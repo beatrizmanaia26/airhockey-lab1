28cff624121362e6058b26a14cef1cd0cd988a42
beatriz <beatrizlberto@gmail.com>
2026-02-11 22:39:07 -0300

    adicionando bases do jogo (script pro botao vermelho, bola se emxer e algumas paredes

---
+++ Assets/PlayerControlls.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class PlayerControlls : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        var pos = transform.position;
+        pos.x = mousePos.x;
+        pos.y = mousePos.y;
+        transform.position = pos;
+    }
+}
